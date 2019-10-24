8f51173f02e84a1a4cb5f39075f384c50d1875ff
MostafaMarandi <most.marandi@gmail.com>
2019-10-24 09:13:06 -0400

    first commit

---
+++ src/comp6461/a1/HTTPClient.java
@@ -0,0 +1,219 @@
+package comp6461.a1;
+
+import java.net.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+ 
+public class HTTPClient {
+     
+    /**
+     * This is main method and it calls the testHTTPLib method.
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+    	testHTTPLib();
+    }
+    
+    /**
+     * This method will extract different information from a command and send it to library.
+     * @throws MalformedURLException
+     */
+    public static void testHTTPLib() throws MalformedURLException {  
+    	HTTPLib hlib = new HTTPLib();
+    	Scanner input = new Scanner(System.in);
+    	String command = input.nextLine();
+    	String[] data = command.split("\\s+");
+    	
+    	//check if the command is help command 
+    	if(data[1].equalsIgnoreCase("help")) {
+    		
+    		//check if the command is general help command 
+    		if(data.length == 2) {
+        		System.out.println("httpc is a curl-like application but supports HTTP protocol only.");
+        		System.out.println("Usage:");
+        		System.out.println("\thttpc command [arguments]");
+        		System.out.println("The commands are:");
+        		System.out.println("\tget executes a HTTP GET request and prints the response.");
+        		System.out.println("\tpost executes a HTTP POST request and prints the response.");
+        		System.out.println("\thelp prints this screen.\n");
+        		System.out.println("Use \"httpc help [command]\" for more information about a command.");
+        	}
+    		//check if the command is get usage command 
+        	else if(data.length == 3 && data[2].equalsIgnoreCase("get")) {
+        		System.out.println("usage: httpc get [-v] [-h key:value] URL\n");
+        		System.out.println("Get executes a HTTP GET request for a given URL.\n");
+        		System.out.println("\t-v              Prints the detail of the response such as protocol, status, and headers.");
+        		System.out.println("\t-h key:value    Associates headers to HTTP Request with the format 'key:value'.");    		
+        	}
+    		//check if the command is post usage command 
+        	else if(data.length == 3 && data[2].equalsIgnoreCase("post")) {
+        		System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n");
+        		System.out.println("Post executes a HTTP POST request for a given URL with inline data or from file.\n");
+        		System.out.println("\t-v              Prints the detail of the response such as protocol, status, and headers.");
+        		System.out.println("\t-h key:value    Associates headers to HTTP Request with the format 'key:value'.");    
+        		System.out.println("\t-d string       Associates an inline data to the body HTTP POST request.");
+        		System.out.println("\t-f file         Associates the content of a file to the body HTTP POST request.\n");
+        		System.out.println("Either [-d] or [-f] can be used but not both.");
+        	}
+    	}
+    	
+    	//check if the command is regular get or post command
+    	else {
+    		String url1 = data[data.length-1];  //it will extract url from the command
+        	URL url = new URL(url1);            //it will create url object to get different information from url 
+        	boolean flag = false;
+        	int counter = 0;
+        	hlib.verbos = false;
+    		
+        	//checks if the command is httpc command or not
+	    	if(data[0].equalsIgnoreCase("httpc")) {
+	    		flag = true;
+	    	}
+	    	//checks get command
+	    	if(data[1].equalsIgnoreCase("get") && flag == true) {
+	    		hlib.operation = "Get";
+	    		for(int i=2;i<(data.length-1);i++) {
+	    			int index = 0;
+	    			//extract different information for get command
+	    			switch(data[i]) {
+	    			case "-v":
+	    				hlib.verbos = true;
+	    				break;
+	    			case "-h":
+	    				index = i;
+	    	            hlib.addHeader(data[index+1]);
+	    	            i += 1;
+	    	            break;
+	    			case "--h":
+	    				index = i;
+	    	            hlib.addHeader(data[index+1]);
+	    	            i += 1;
+	    	            break;  
+	    	        default:
+	    	        	flag = false;
+	    			}
+	    			
+	    				
+	    		}
+	    		hlib.host = url.getHost();
+	        	hlib.port = "80";
+	            hlib.path = url.getPath();
+	            hlib.addData(url.getQuery());
+	    		
+	    	}
+	    	//checks post command
+	    	else if(data[1].equalsIgnoreCase("post") && flag == true) {
+	    		hlib.operation = "Post";
+	    		for(int i=2;i<(data.length-1);i++) {
+	    			int index = 0;
+	    			//extract different information for post command
+	    			switch(data[i]) {
+	    			case "-v":
+	    				hlib.verbos = true;
+	    				break;
+	    			case "-h":
+	    				index = i;
+	    	            hlib.addHeader(data[index+1]);
+	    	            i += 1;
+	    	            break;
+	    			case "--h":
+	    				index = i;
+	    	            hlib.addHeader(data[index+1]);
+	    	            i += 1;
+	    	            break;  
+	    			case "-f":
+	    				index = i;
+	    				hlib.dataFile = data[index+1];
+	    				i += 1;
+	    				break;
+	    			case "--f":
+	    				index = i;
+	    				hlib.dataFile = data[index+1];
+	    				i += 1;
+	    				break;
+	    			case "-d":
+	    				index = i;
+	    	            hlib.addData(data[index+1]);
+	    	            i += 1;
+	    	            break;
+	    			case "--d":
+	    				index = i;
+	    	            hlib.addData(data[index+1]);
+	    	            i += 1;
+	    	            break;
+	    			case "-o":
+	    				index = i;
+	    				hlib.save = true;
+			    		hlib.outputFile = data[index+1];
+			    		i += 1;
+	    	            break;
+	    			case "--o":
+	    				index = i;
+	    				hlib.save = true;
+			    		hlib.outputFile = data[index+1];
+			    		i += 1;
+	    	            break;
+	    	        default:
+	    	        	flag = false;
+	    			}
+	    				
+	    		}
+	    		hlib.host = url.getHost();
+	        	hlib.port = "80";
+	            hlib.path = url.getPath();	
+	    	}
+	    	//it checks if any information is missing or command is wrong
+	    	else {
+	    		flag = false;
+	    	}
+	    	
+	    	//it checks whether -d or -f is used at same time or not
+	    	if((Arrays.asList(data).contains("-f") || Arrays.asList(data).contains("--f")) && (Arrays.asList(data).contains("-d") || Arrays.asList(data).contains("--d"))) {
+	    		System.out.println("Either [-d] or [-f] can be used but not both.");
+	    		flag = false;
+	    	}
+	    	
+	    	//if every thing is ok then it will send the request to library 
+	    	if(flag==true) {
+	    		try {
+	    			hlib.sendHTTPRequest();
+	    		} catch (IOException e) {
+	    			e.printStackTrace();
+	    		}
+	    	}
+	    	else {
+		    	System.out.println("Your command is wrong...!!! Please enter right command.");
+	    	}
+    	}
+    }
+}
+
+
+/*
+ 
+ 
+httpc help 
+
+httpc help post
+
+httpc help get
+
+httpc get http://httpbin.org/get?course=networking&assignment=1
+
+httpc get -v http://httpbin.org/get?course=networking&assignment=1
+
+httpc post -h Content-Type:application/json --d {"Assignment":1} http://httpbin.org/post
+
+httpc post -h Content-Type:application/json --d {"Assignment":1} -d {"Assignment":2} --d {"Assignment":3} http://httpbin.org/post
+
+httpc post -h Content-Type:application/json -f {} -d {} http://httpbin.org/post
+
+httpc post -h Content-Type:application/json -f data.txt http://httpbin.org/post
+
+httpc post -h Content-Type:application/json -d {"Assignment":1} -o result.txt http://httpbin.org/post
+
+httpc get -v -h Content-Type:application/json https://httpstat.us/302
+ 
+ */
---
+++ src/comp6461/a1/HTTPLib.java
@@ -0,0 +1,373 @@
+package comp6461.a1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class HTTPLib {
+	
+	//====================================================================== fields members
+	String request;
+	Socket socket;
+	PrintWriter writer;
+	BufferedReader reader;
+	public String operation; 
+	public String host;
+	public String port;
+	public String path;
+	public String outputFile;
+	public String dataFile;
+	public boolean verbos;
+	public boolean save;
+	public ArrayList<String> datas;
+	public ArrayList<String> headers;
+	int redirectCount;
+
+	//======================================================================= constructor
+	public HTTPLib() {
+		outputFile = "";
+		dataFile = "";
+		headers = new ArrayList<String>();
+		datas = new ArrayList<String>();
+		redirectCount = 0;
+	}
+	
+	//======================================================================= function members
+	
+	/**
+	 * This method is responsible for adding header token to header Map
+	 * @param header: header token
+	 */
+	public void addHeader(String header) {
+		//check if the header exist before
+		boolean match = false;
+		for(String h : this.headers) {
+			if(h.equalsIgnoreCase(header)) {
+				match = true;
+				break;
+			}
+		}
+		if(!match) {
+			this.headers.add(header);
+		}
+	}
+	
+	/**
+	 * This method is responsible for adding data token
+	 * @param data: data token
+	 */
+	public void addData(String data) {		
+		this.datas.add(data);				
+	}
+	
+	/**
+	 * This method is responsible for removing header from hear Map
+	 * @param key: header token
+	 */
+	public void removeHeader(String header) {
+		for (int i = 0; i < this.headers.size(); i++) {
+			if(this.headers.get(i).equalsIgnoreCase(header)) {
+				this.headers.remove(i);
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * This method is responsible for sending HTPP request
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public void sendHTTPRequest() throws UnknownHostException, IOException {
+		this.redirectCount = 0;
+		sendRequest();
+	}
+	
+	/**
+	 * This method is responsible for sending HTPP request
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	void sendRequest() throws UnknownHostException, IOException {
+	    this.socket = new Socket(this.host, Integer.parseInt(this.port));
+	    
+	    // GET operation
+	    if (this.operation.toUpperCase().equals("GET")) {
+	    	
+		  // append request operation and path
+		  this.request = "GET " + this.path;
+		  
+		  // append request data
+		  if(!this.dataFile.isEmpty()) {
+			  if(!readDataFromFile()) {
+				  System.out.println("the file for input data is not found !!!!");
+				  socket.close();
+				  return;
+			  }
+		  }
+		  
+		  if(this.datas.size() > 0  ) {
+			  this.request = this.request + "?";
+			  boolean first = true;
+			  for(String data : this.datas) {
+				  if(first) {
+					  first = false;
+				  }
+				  else {
+					  this.request = this.request + "&";
+				  }
+				  this.request = this.request + data;  
+			  }
+		  }
+
+		  // append HTTP protocol version
+		  this.request = this.request + " HTTP/1.0\r\n";		  
+		  
+		  // append HTTP headers
+		  addHeader("Host:" + this.host);
+		  appendHeader();
+		  
+		  // terminate HTTP request with an empty line
+		  this.request = this.request + "\r\n";
+	    } 
+	    
+	    // POST operation
+	    else if (this.operation.toUpperCase().equals("POST")) {
+	    	
+    	  // get request data
+	      if(!this.dataFile.isEmpty()) {
+	    	  if(!readDataFromFile()) {
+				  System.out.println("the file for input data is not found !!!!");
+				  socket.close();
+				  return;
+			  }
+	      }
+	      
+	      // append request operation , path and HTTP protocol version
+	      this.request = "POST " + this.path + " HTTP/1.0\r\n";
+	     
+	      // append HTTP headers
+	      addHeader("Host:" + this.host);
+	      //addHeader("Content-Length:" + String.valueOf(this.datas.length()));
+	      addHeader("Content-Length:" + this.datas.stream().mapToInt(i -> i.length()).sum());	   
+	      appendHeader();
+	      
+	      // terminate HTTP request with an empty line
+	      this.request = this.request + "\r\n";	      	      
+	      
+	      //append data tokens
+	      //this.request = this.request + this.datas;
+	      boolean first = true;
+	      for(String data : this.datas) {
+	    	  if(first) {
+				  first = false;
+			  }
+			  else {
+				  this.request = this.request + "\r\n";
+			  }
+			  this.request = this.request + data;  
+		  }
+	    } 
+	    
+	    // not a valid HTTP operation
+	    else {
+	      System.out.println("Invalid HTTP operation !!!");
+	      socket.close();
+	      return;
+	    }
+	    
+	    // send HTTP request
+	    writeRequest();
+	    
+	    // print HTTP response
+	    printResponse();
+
+	    // close the socket
+	    socket.close();
+	  }
+	
+		
+	
+	/**
+	 * This method is responsible for append headers in header Map to the HTTP request
+	 */
+	void appendHeader() {
+		for(String h : this.headers) {
+			this.request = this.request + h + "\r\n";
+		}
+	}
+	
+	/**
+	 * This method is responsible for printing the response in the console
+	 */
+	void printResponse() {
+		try {			
+		    this.reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));		    
+			String line; 
+	        boolean isResponseContent = false;
+	        String response = "";
+	        String newURL = "";
+	        
+	        if(!this.save) {
+		        while ((line = this.reader.readLine()) != null) {		        	
+		        	response = response + line + "\r\n";
+		        	
+		        	if(this.verbos || isResponseContent) {
+		        		System.out.println(line);
+		        	}
+		        	else {
+		        		if(line.length() == 0) {
+		        			isResponseContent = true;
+		        		}
+		        	}
+		        }
+	        }
+	        else {
+	        	File file = new File(this.outputFile);
+			    PrintWriter fileWriter = new PrintWriter(file);	        	
+	        	while ((line = this.reader.readLine()) != null) {	        		
+	        		response = response + line + "\r\n";
+		        	
+	        		if(this.verbos || isResponseContent) {
+		        		System.out.println(line);
+		        		fileWriter.println(line);
+		        	}
+		        	else {
+		        		if(line.length() == 0) {
+		        			isResponseContent = true;
+		        		}
+		        	}
+			    }
+	        	
+		        fileWriter.flush();
+		        fileWriter.close();
+	        }	        
+	        reader.close();
+	        
+	        // check for redirect
+	        if(response.subSequence(response.indexOf(" ") + 1, response.indexOf(" ") + 2).equals("3")) {
+	        	
+	        	//check http request type
+	        	if(this.operation.equalsIgnoreCase("GET")) {
+		        	// check redirection count
+		        	if(this.redirectCount < 5) {	        		
+		        		this.redirectCount ++;
+		        		
+		    			int locationIndex = response.indexOf("Location:");
+		    			if(locationIndex != -1) {
+		    				int index = response.indexOf("Location:")+10;
+		        			while(response.charAt(index) != '\n') {
+		        				newURL = newURL + String.valueOf(response.charAt(index));
+		        				index ++;
+		        			}
+		
+		        			System.out.println();
+		        			System.out.println(":::::: Redirect [" + this.redirectCount + "] to");
+		        			System.out.println(newURL);
+		        			System.out.println();
+		        			
+		        			//int pathIndex = newURL.indexOf("/");
+		//        			System.out.println("host: "+ newURL.substring(0,pathIndex != -1 ? pathIndex : newURL.length()));
+		//        			this.host = newURL.substring(0,pathIndex != -1 ? pathIndex : newURL.length());
+		//        			if(pathIndex != -1) {
+		////        				System.out.println("path: "+ newURL.substring(pathIndex,newURL.length()));
+		//        				this.path = newURL.substring(pathIndex,newURL.length());
+		//        			}
+		//        			else{
+		//        				this.path = "/";
+		//        			}
+		        			
+		        			this.host = new URL(newURL).getHost();
+		        			this.path = new URL(newURL).getPath();
+		        			if(this.path.contentEquals("")) {
+		        				this.path = "/";
+		        			}
+		        			
+		        			sendRequest();
+		    			}
+		    			else {
+		    				System.out.println();
+							System.out.println(":::::: Redirect [" + this.redirectCount + "] to");
+							System.out.println("new URL is not provided in the response !!!");
+						}
+		        	}
+		        	else {
+		        		this.redirectCount = 0;
+		        		System.out.println();
+		        		System.out.println(":::::: Redirect [" + this.redirectCount + "] to");
+						System.out.println("maximum number of redirection is reached !!!");
+		        	}
+	        	}
+	        	else {
+	        		this.redirectCount = 0;
+	        		System.out.println();
+	        		System.out.println(":::::: Redirect [" + this.redirectCount + "] to");
+					System.out.println("for POST request the automatice redirection is not permited!!!");
+	        	}
+    			
+    		}
+		} 
+		catch (Exception e) {
+
+		}
+		finally {
+			try {
+				if(this.reader != null)
+					this.reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+		
+	/**
+	 * This method is responsible for send Http request
+	 */
+	void writeRequest() {
+		try {			
+			this.writer = new PrintWriter(this.socket.getOutputStream(), true);		    
+			this.writer.write(this.request);
+		    this.writer.flush();
+		    System.out.println("============");
+		    System.out.println("this.request: "+ this.request );
+		    System.out.println("============");
+		} 
+		catch (Exception e) {
+
+		}
+	}
+
+	/**
+	 * This function is responsible for reading data from file 
+	 * @return boolean: if the file exist the return true else false
+	 * @throws IOException
+	 */
+	boolean readDataFromFile() throws IOException {
+	  File f = new File(this.dataFile); 	
+	  if(!f.exists()) {
+		  
+		  return false;
+	  }
+	  
+	  BufferedReader br = new BufferedReader(new FileReader(f)); 			  
+	  String st; 
+	  datas = new ArrayList<String>();
+	  while ((st = br.readLine()) != null) {
+		  this.datas.add(st);
+	  }
+	  return true;
+	}
+}
