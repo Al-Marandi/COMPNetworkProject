ff79dfed41b9b97eaac15eb69cc2a3de4d95c855
Divyansh Thakar <43654517+Divyansh88@users.noreply.github.com>
2019-11-24 16:32:02 -0500

    Updated FMSClient

--- src/comp6461/a3/FMSClient.java
+++ src/comp6461/a3/FMSClient.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -93,34 +97,73 @@ public class FMSClient {
 	 * @throws URISyntaxException
 	 */
 	public synchronized static void main(String args[]) throws IOException, URISyntaxException {
-		BufferedReader breader = new BufferedReader(new InputStreamReader(System.in));
-		boolean contentFlag = false, headerFlag = false;
-		String content = "", query, url = "";
-		ArrayList<String> headers = new ArrayList<>();
-		String fsClient = breader.readLine();
-		String[] cClient = fsClient.split(" ");
-		if(cClient[0].equals("httpfs")) {
-			for(int i =0; i<cClient.length; i++) {
-				if(cClient[i].startsWith("-d")) {
-					contentFlag = true;
-					content = cClient[i+1];
-				}
-				if(cClient[i].startsWith("http://")){
-					url = cClient[i];
-				}
-				if(cClient[i].equals("-h")) {
-					headerFlag = true;
-					headers.add(cClient[i+1]);
-				}
-			}
+//		BufferedReader breader = new BufferedReader(new InputStreamReader(System.in));
+//		boolean contentFlag = false, headerFlag = false;
+//		String content = "", query, url = "";
+//		ArrayList<String> headers = new ArrayList<>();
+//		String fsClient = breader.readLine();
+//		String[] cClient = fsClient.split(" ");
+//		if(cClient[0].equals("httpfs")) {
+//			for(int i =0; i<cClient.length; i++) {
+//				if(cClient[i].startsWith("-d")) {
+//					contentFlag = true;
+//					content = cClient[i+1];
+//				}
+//				if(cClient[i].startsWith("http://")){
+//					url = cClient[i];
+//				}
+//				if(cClient[i].equals("-h")) {
+//					headerFlag = true;
+//					headers.add(cClient[i+1]);
+//				}
+//			}
+//		}
+//		URI uri = new URI(url);
+//		String host = uri.getHost();
+//		query = uri.getPath();
+//		int port = uri.getPort();
+//		
+//		FMSClient client = new FMSClient(host, port, query.substring(1), content, headers, contentFlag, headerFlag);
+		DatagramSocket aSocket = null; 	
+		try{
+			String msg = "Hi";
+			System.out.println("Client started........");
+			System.out.println("Client sent : "+msg);
+			aSocket = new DatagramSocket();
+			byte [] message = msg.getBytes();
+			
+			InetAddress routerAddress = InetAddress.getByName("localhost"); 			
+			int routerPort = 3000;
+			InetAddress serverAddress = InetAddress.getByName("localhost"); 			
+			int serverPort = 8000;
+			
+			Packet responsePacket = new Packet(0, 1, serverAddress, serverPort, message);
+			byte[] requestData = responsePacket.toBytes();
+			DatagramPacket requestUDPacket = new DatagramPacket(requestData, requestData.length, routerAddress, routerPort);
+			aSocket.send(requestUDPacket);
+			
+			byte [] buffer = new byte[Packet.MAX_LEN];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			
+			//Client waits until the reply is received-----------------------------------------------------------------------
+			aSocket.receive(reply);//reply received and will populate reply packet now.
+			byte[] rawData = reply.getData();
+			Packet replyPacket = Packet.fromBytes(rawData);
+			String payload = new String(replyPacket.getPayload());
+			System.out.println("Reply received from the server is: "+ payload.trim());//print reply message after converting it to a string from byte
+			
 		}
-		URI uri = new URI(url);
-		String host = uri.getHost();
-		query = uri.getPath();
-		int port = uri.getPort();
-		
-		FMSClient client = new FMSClient(host, port, query.substring(1), content, headers, contentFlag, headerFlag);
-//		client.request();
+		catch(SocketException e){
+			System.out.println("Socket: "+e.getMessage());
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.out.println("IO: "+e.getMessage());
+		}
+		finally{
+			if(aSocket != null) aSocket.close();//now all resources used by the socket are returned to the OS, so that there is no
+												//resource leakage, therefore, close the socket after it's use is completed to release resources.
+		}
 	}
 }
 
